b9f4b5cd59721223a2d97b9d7380eceb7b2eac0f
amrya <microsoftuseslinux@gmail.com>
2016-05-24 15:10:45 +0530

    changed ports

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -97,8 +97,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              = 42080;
-const int      RPC_DEFAULT_PORT                              = 42081;
+const int      P2P_DEFAULT_PORT                              = 42136;   // changed 42080
+const int      RPC_DEFAULT_PORT                              = 42121;   // changed 42081
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
